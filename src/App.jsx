7549fb2cca0ef2f07baf10df66bc6e6ec36735a4
unknown <jaiswalsanket244@gmail.com>
2024-07-15 18:31:10 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from 'react';
+import HomePage from './Pages/HomePage';
+import Navbar from './Pages/Navbar';
+import LoginSignupPage from './Pages/Login';
+import App_routes from './Pages/App_routes';
+import { Box, Button, Text, useDisclosure } from '@chakra-ui/react';
+
+function App() {
+  const [rendernav, setrendernav] = useState(false);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  // Automatically open the message on mount
+  useEffect(() => {
+    onOpen();
+  }, []);
+
+  return (
+    <div style={{ position: 'relative' }}>
+       {isOpen && (
+        <Box
+          position="absolute"
+          top="0"
+          left="0"
+          right="0"
+          bg="yellow.200"
+          p={3}
+          textAlign="center"
+          borderBottomWidth="1px"
+          borderColor="yellow.300"
+          zIndex={10}
+        >
+          <Text fontSize="md">
+            The server might take up to 5 minutes to load. Please wait when we see loading indicator...Thanks
+          </Text>
+          <Button
+            size="sm"
+            colorScheme="teal"
+            onClick={onClose}
+            ml={4}
+          >
+            Dismiss
+          </Button>
+        </Box>
+      )}
+      <Navbar rendernav={rendernav} />
+      <App_routes rendernav={rendernav} setrendernav={setrendernav} />
+    </div>
+  );
+}
+
+export default App;
